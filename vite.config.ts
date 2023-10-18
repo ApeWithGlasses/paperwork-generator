12d2c58a2edee7ec95a7b3a6a48382d959fdb486
ApeWithGlasses <wcgiraldo0@gmail.com>
2023-10-18 02:00:49 -0500

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: 'https://apewithglasses.github.io/paperwork-generator/'
+  plugins: [react()]
 })
